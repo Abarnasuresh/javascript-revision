5c57d7088aa3bf8c675a5d11eed19598566e943b
Abarna <abarnasuresh20012gmail.com>
2022-04-22 17:28:09 +0530

    js

---
+++ arrays/lastindexof.js
@@ -0,0 +1,7 @@
+const animals = ['dog', 'Tiger', 'Penguin', 'Dog'];
+
+console.log(animals.lastIndexOf('Dog'));
+
+
+console.log(animals.lastIndexOf('Tiger'));
+
---
+++ reverse.js
@@ -0,0 +1,7 @@
+const array1 = ['one', 'two', 'three'];
+console.log('array1:', array1);
+
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);               //"array1:" Array ["one", "two", "three"]
+                                                //"reversed:" Array ["three", "two", "one"]
