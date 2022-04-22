6fc69eca819aacd741d0925e3f8e9f169432039d
Abarna <abarnasuresh20012gmail.com>
2022-04-22 16:28:16 +0530

    js

---
+++ arrays/includes.js
@@ -0,0 +1,5 @@
+const a = ['abc', 'xyz', 'efd'];
+
+console.log(a.includes('abc'));             //abc
+
+                                        
---
+++ arrays/indexof.js
@@ -0,0 +1,4 @@
+const colors = ['red', 'blue', 'purple', 'gray', 'black'];
+
+console.log(colors.indexOf('blue'));                          //1
+  
