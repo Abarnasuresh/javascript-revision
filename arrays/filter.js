3e44601eb966c3e8656bf2511e820723b6d376ad
Abarna <abarnasuresh20012gmail.com>
2022-04-22 16:17:02 +0530

    js

--- arrays/filter.js
+++
@@ -1,5 +0,0 @@
-const words = ['add','sub','multiply','div'];
-
-const result = words.filter(word => word.length > 4);
-
-console.log(result);     //[ 'multiply' ]
---
+++ arrays/prototype.filter.js
@@ -0,0 +1,5 @@
+const words = ['abarna', 'ramya', 'harini', 'anu', 'dharani', 'harsha'];
+
+const result = words.filter(word => word.length > 5);
+
+console.log(result);                                     //[ 'abarna', 'harini', 'dharani', 'harsha' ]
