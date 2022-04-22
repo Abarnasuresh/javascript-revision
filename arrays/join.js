a1ececc0039012876c5cccc6d76321ead1cdcfb4
Abarna <abarnasuresh20012gmail.com>
2022-04-22 16:39:14 +0530

    js

---
+++ arrays/join.js
@@ -0,0 +1,7 @@
+const names = ['abi', 'ramya', 'harini'];
+
+console.log(names.join());
+
+
+console.log(names.join(''));                     //abi,ramya,harini
+                                                 //abiramyaharini  
