d20465e29e880dbff36435996114ef50c1e4aa04
Abarna <abarnasuresh20012gmail.com>
2022-04-27 17:23:57 +0530

    js

---
+++ Promises/promiseallsettled.js
@@ -0,0 +1,23 @@
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The first promise has resolved');
+        resolve(10);
+    }, 1 * 1000);
+
+});
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The second promise has rejected');
+        reject(20);
+    }, 2 * 1000);
+});
+
+Promise.allSettled([p1, p2])
+    .then((result) => {
+        console.log(result);
+    });                                                         //The first promise has resolved
+                                                                //The second promise has rejected
+                                                                //Array(2)
+                                                                    //{status:"fulfilled",value:10}
+                                                                    //{status:"rejected",reason:20}
