b0be3ce4d995a8afdb5b8ffd0760e5ca79bd6302
Abarna <abarnasuresh20012gmail.com>
2022-04-28 15:56:11 +0530

    js

---
+++ Class/class.js
@@ -0,0 +1,10 @@
+class Car {                          //Creating a class 
+    constructor(name, year) {          
+      this.name = name;
+      this.year = year;
+    }
+  }
+  let myCar1 = new Car("Ford", 2014);    //instance of class
+  console.log(myCar1)
+  
+  //Car { name: 'Ford', year: 2014 }
--- Spreadoperator/spreadoperator with object.js
+++ Spreadoperator/spreadoperator with object.js
@@ -4,4 +4,4 @@ const obj2 = { c : 3 };
 // add members obj1 and obj2  to obj3
 const obj3 = {...obj1, ...obj2};
 
-console.log(obj3); // {a: 1, b: 2, c: 3}
+console.log(obj3); // {a: 1, b: 2, c: 3} 
