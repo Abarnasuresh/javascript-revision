c7ed9f4938196c3c996de3a0214baeaaeb478602
Abarna <abarnasuresh20012gmail.com>
2022-05-03 09:46:05 +0530

    js

--- javascript runtime/eventloop.js
+++ javascript runtime/eventloop.js
@@ -1,3 +1,17 @@
 //event loop is the secret behind JavaScript's asynchronous programming.        
 //single threaded programming language means that js can do only one thing at a single point in time.
 
+function task(message) {
+    // emulate time consuming task
+    let n = 10000000000;
+    while (n > 0){
+        n--;
+    }
+    console.log(message);
+}
+
+console.log('Start script...');
+task('Call an API');
+console.log('Done!');                              //Start script...
+                                                   //Call an API
+                                                   //Done!
---
+++ javascript runtime/variablescopes.js
@@ -0,0 +1,10 @@
+var message = 'Hi';
+
+function say() {
+    var message = 'Hello';
+    console.log('message');
+}
+
+say();
+console.log(message);                                 //Hello
+                                                      //Hi
